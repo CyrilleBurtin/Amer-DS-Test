3b3a551917c84912b9f12bf365c9b20e82002eae
Cyrille <cyrille.burtin@me.com>
2024-08-01 10:58:42 +0200

    [feat] eslint fix

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,7 +4,7 @@ import ProductCard from './ProductCard';
 
 function App() {
   return (
-    <div className="w-full h-lvh flex items-center justify-center bg-gray-200 ">
+    <div className="flex h-lvh w-full items-center justify-center bg-gray-200 ">
       {PRODUCTS_MOCK.map((product: Product) => (
         <ProductCard {...product} key={product.description} />
       ))}
